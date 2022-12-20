6fe8e772214f6d56fcf3d0070900eefbe69f7bd3
fcole90 <fcole90@gmail.com>
2022-12-20 21:16:37 +0100

    Enable automatic deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 const withMDX = require('@next/mdx')({
