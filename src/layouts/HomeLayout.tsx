b9e7b5c667e6aac017b453803dc76ad6ea43ff4a
fcole90 <fcole90@gmail.com>
2022-12-29 20:25:06 +0100

    Use MDX for index

---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,66 @@
+import { Inter } from '@next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+interface Props {
+  meta: {
+    title: string,
+    links: {
+      title: string,
+      subtitle: string,
+      href: string,
+    }[]
+  },
+  children: React.ReactNode
+}
+
+const HomeLayout: React.FC<Props> = ({ meta }) => (
+  <main className={styles.main}>
+    <div className={styles.center}>
+      <h1 className={inter.className}>{meta.title}</h1>
+    </div>
+
+    <div className={styles.center}>
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+      <div className={styles.thirteen}>
+        <Image
+          src="/thirteen.svg"
+          alt="13"
+          width={40}
+          height={31}
+          priority
+        />
+      </div>
+    </div>
+
+    <div className={styles.grid}>
+      {meta.links.map(({ title, subtitle, href }, index) => (
+        <Link
+          key={index}
+          href={href}
+          className={styles.card}
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            {title} <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            {subtitle}
+          </p>
+        </Link>
+      ))}
+    </div>
+  </main>
+)
+
+export default HomeLayout
