d655dc5dff8d8630a85a28d00c90a58b40e3e3db
fcole90 <fcole90@users.noreply.github.com>
2022-12-30 10:23:19 +0000

    deploy: dbbbb0b4d6b7cbffbaeada416e2e46eba04fe4b3

--- _next/static/vrl0NZSIH8OuvAHa4SxZf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/507-c64c39d609755259.js","static/css/52ad744436347352.css","static/chunks/pages/index-d7dcb22be4b7a5bd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
